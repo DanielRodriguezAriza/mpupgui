b7c64918e873b63adfae226e88fe3581fcff71b9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-05 20:21:31 +0100

    Created compiler menu

---
+++ mpupgui/lib/menus/menu_fproc_compiler.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mpupgui/widgets/file_processor.dart';
+
+class CompilerMenu extends StatelessWidget {
+  const CompilerMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const FileProcessor();
+  }
+}
+
