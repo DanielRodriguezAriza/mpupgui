fdc3fcc4546182577d9d9c16605db0f9eda351e5
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-05 17:39:34 +0100

    Created widget for main screen
    NOTE : This widget may need to be removed, as there is no "main" screen idea as of now. Maybe this could be a "greeting" or "what's new" or "MOTD" screen of sorts. The idea would be to have a screen for each tab in the program, so this is low priority.

---
+++ mpupgui/lib/screens/screen_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScreenMain extends StatefulWidget {
+  const ScreenMain({super.key});
+
+  @override
+  State<ScreenMain> createState() => _ScreenMainState();
+}
+
+class _ScreenMainState extends State<ScreenMain> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
