2cbf5cc65514d6e70a58fad3b922ec04402bffba
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-05 22:26:24 +0100

    Created theme manager class

---
+++ mpupgui/lib/data/theme_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ThemeManager {
+  // TODO : Implement!
+  static Color getColor(int level) {
+    return Colors.blue; // For now, always return this color...
+  }
+}
