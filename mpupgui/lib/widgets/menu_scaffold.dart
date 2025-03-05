f87a9bc93bfd109bba4120ee01b8bbaed7c9b1e9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-05 22:28:13 +0100

    Created custom MenuScaffold widget class
    This class will standardise the scaffold structure for menus within the app.

---
+++ mpupgui/lib/widgets/menu_scaffold.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../data/theme_manager.dart';
+
+// Wrapper class for Scaffold which will helps us define scaffolds in a consistent
+// manner across the app.
+
+class MenuScaffold extends StatelessWidget {
+
+  final String title;
+  final Widget body;
+
+  const MenuScaffold({
+    super.key,
+    required this.title,
+    required this.body
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return createScaffold();
+  }
+
+  Widget createScaffold() {
+    return Scaffold(
+      appBar: createAppBar(),
+      body: createBody(),
+      backgroundColor: ThemeManager.getColor(0),
+    );
+  }
+
+  AppBar createAppBar() {
+    return AppBar(
+      title: Text(
+        title
+      )
+    );
+  }
+
+  Widget createBody() {
+    return Container(
+      child : body
+    );
+  }
+}
+
