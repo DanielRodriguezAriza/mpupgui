a960f02b15f7de07cf9c36263c55cb92401834ae
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-05 22:27:12 +0100

    Implemented basic init logic

---
+++ mpupgui/lib/menus/menu_init.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mpupgui/data/menu_manager.dart';
+
+// Widget menu class responsible for handling program initialization related
+// logic and operations.
+
+class InitScreen extends StatefulWidget {
+  const InitScreen({super.key});
+
+  @override
+  State<InitScreen> createState() => _InitScreenState();
+}
+
+class _InitScreenState extends State<InitScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      MenuManager.loadMenu(context, "/main"); // Start by loading the "main" menu.
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black // Shows a simple black screen while the initialization process is taking place.
+    );
+  }
+}
+
