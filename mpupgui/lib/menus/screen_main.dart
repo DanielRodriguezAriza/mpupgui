724d0b84df2accb6b0a4aa47bd158f3bceab3058
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-05 20:21:10 +0100

    Renamed screens directory to menus

---
+++ mpupgui/lib/menus/screen_main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// TODO : Get rid of this maybe? Or make this a "what's new" kind of screen.
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
