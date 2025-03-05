0c13d1dff2ff9580951e84f25d23d568850c5ce3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-05 20:20:41 +0100

    Created widget for file processing menus

---
+++ mpupgui/lib/widgets/file_processor.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// NOTE : This widget class is responsible for representing a generic menu
+// with the purpose of processing a type of file.
+// The decompiler and compiler menus / screens are the ones that make use of
+// this type of widget.
+
+class FileProcessor extends StatefulWidget {
+  const FileProcessor({super.key});
+
+  @override
+  State<FileProcessor> createState() => _FileProcessorState();
+}
+
+class _FileProcessorState extends State<FileProcessor> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
