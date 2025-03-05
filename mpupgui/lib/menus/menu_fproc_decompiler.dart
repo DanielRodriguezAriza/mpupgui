8f809495382d9da4e11bd9e20830d6fdc68341ff
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-05 20:21:23 +0100

    Created decompiler menu

---
+++ mpupgui/lib/menus/menu_fproc_decompiler.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mpupgui/widgets/file_processor.dart';
+
+class DecompilerMenu extends StatelessWidget {
+  const DecompilerMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const FileProcessor();
+  }
+}
+
