5312c5ff94e685bfec19d5af2cc13487e8f64edb
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-06 02:03:10 +0100

    Added customization settings to mpup button class

--- mpupgui/lib/widgets/mpup_button.dart
+++ mpupgui/lib/widgets/mpup_button.dart
@@ -5,17 +5,33 @@ class MagickaPupButton extends StatelessWidget {
 
   final String text;
   final Function onPressed;
+  final double sizeX;
+  final double sizeY;
+  final double borderRadius;
+  final double elevation;
 
   const MagickaPupButton({
     super.key,
     required this.text,
-    required this.onPressed
+    required this.onPressed,
+    this.sizeX = 250,
+    this.sizeY = 50,
+    this.borderRadius = 5,
+    this.elevation = 1.4, // Values between 1 and 3 look pretty nice, picking this value for now.
   });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: (){onPressed();},
+      style: ElevatedButton.styleFrom(
+        // backgroundColor: ThemeManager.getColor(0), // TODO : Implement colors first and then enable this setting...
+        elevation: elevation,
+        fixedSize: Size(sizeX, sizeY),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius)
+        )
+      ),
       child: MagickaPupText(text: text),
     );
   }
