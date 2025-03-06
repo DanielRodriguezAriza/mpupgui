9f99f9a024661950780716403b7ae8fe22dc88ba
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-06 16:18:43 +0100

    Modified ThemeManager to support color map with themed element types dicts

--- mpupgui/lib/data/theme_manager.dart
+++ mpupgui/lib/data/theme_manager.dart
@@ -14,59 +14,63 @@ class ThemeManager {
 
   static Theme currentTheme = Theme.dark;
 
-  // App colors
-  static const Map<Theme, List<Color>> colorsApp = {
-    Theme.dark : [
-      Color.fromARGB(255, 23, 23, 23),
-      Color.fromARGB(255, 43, 43, 43),
-      Color.fromARGB(255, 54, 54, 54),
-      Color.fromARGB(255, 128, 128, 128),
-    ],
-    Theme.light : [
-      Color.fromARGB(255, 53, 53, 53),
-      Color.fromARGB(255, 73, 73, 73),
-      Color.fromARGB(255, 120, 120, 120),
-      Color.fromARGB(255, 160, 160, 160),
-    ]
+  static const Map<ThemeType, Map<Theme, List<Color>>> colorsApp = {
+    ThemeType.image : {
+      Theme.dark: [
+        Color.fromARGB(255, 23, 23, 23),
+        Color.fromARGB(255, 43, 43, 43),
+        Color.fromARGB(255, 54, 54, 54),
+        Color.fromARGB(255, 128, 128, 128),
+      ],
+      Theme.light: [
+        Color.fromARGB(255, 53, 53, 53),
+        Color.fromARGB(255, 73, 73, 73),
+        Color.fromARGB(255, 120, 120, 120),
+        Color.fromARGB(255, 160, 160, 160),
+      ]
+    },
+    ThemeType.text : {
+      Theme.dark: [
+        Color.fromARGB(255, 255, 255, 255),
+      ],
+      Theme.light: [
+        Color.fromARGB(255, 0, 0, 0),
+      ]
+    }
   };
 
   static void setTheme(Theme theme) {
     currentTheme = theme;
   }
 
-  static int getMinValidColorIndex() {
-    return 0;
-  }
-
-  static int getMaxValidColorIndex() {
-    if(colorsApp.isEmpty) {
-      return 0;
+  static bool isValidColorIndex(ThemeType themeType, Theme theme, int idx) {
+    int length = colorsApp[themeType]![theme]!.length;
+    if(length <= 0) {
+      return false;
     }
-
-    int currentLength = colorsApp.entries.first.value.length;
-
-    for(var map in colorsApp.entries) {
-      if(currentLength > map.value.length) {
-        currentLength = map.value.length;
-      }
+    if(idx < 0 || idx >= length) {
+      return false;
     }
-
-    return currentLength - 1;
+    return true;
   }
 
-  static bool isValidColorIndex(int idx) {
-    return (idx >= getMinValidColorIndex() && idx <= getMaxValidColorIndex());
-  }
-
-  static Color getColorByTheme(Theme theme, int level) {
-    if(!isValidColorIndex(level)) {
+  static Color getColorInternal(ThemeType themeType, Theme theme, int level) {
+    if(!isValidColorIndex(themeType, theme, level)) {
       return Colors.black;
     }
-    return colorsApp[theme]![level];
+    return colorsApp[themeType]![theme]![level];
   }
 
-  static Color getColor(int level) {
-    return getColorByTheme(currentTheme, level);
+  static Color getColor(ThemeType themeType, int level) {
+    return getColorInternal(themeType, currentTheme, level);
+  }
+
+  static Color getColorImage(int level) {
+    return getColor(ThemeType.image, level);
+  }
+
+  static Color getColorText(int level) {
+    return getColor(ThemeType.text, level);
   }
 
 }
