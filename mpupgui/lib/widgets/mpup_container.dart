cd058a846571c0527730f07156703d12c289a002
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-06 22:30:12 +0100

    Separated parent and child padding into 2 separate variables
    Note: Maybe it would make more sense to only have a single padding bar and add it to all of the Mpup widget classes. That padding should be refered to the padding between the widget and its parent (what is known here as "paddingParent"). That way, if the child object needs padding, it provides it itself.

--- mpupgui/lib/widgets/mpup_container.dart
+++ mpupgui/lib/widgets/mpup_container.dart
@@ -5,21 +5,23 @@ class MagickaPupContainer extends StatelessWidget {
 
   final Widget child;
   final int colorIndex;
-  final double padding;
+  final double paddingParent;
+  final double paddingChild;
   final double borderRadius;
 
   const MagickaPupContainer({
     super.key,
     required this.child,
     this.colorIndex = 1,
-    this.padding = 15,
+    this.paddingParent = 15,
+    this.paddingChild = 0,
     this.borderRadius = 3,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.all(padding),
+        padding: EdgeInsets.all(paddingParent),
         child: LayoutBuilder(
             builder: (BuildContext context, BoxConstraints constraints) {
               return Container(
@@ -30,7 +32,7 @@ class MagickaPupContainer extends StatelessWidget {
                   borderRadius: BorderRadius.circular(borderRadius),
                 ),
                 child: Padding(
-                    padding: EdgeInsets.all(padding),
+                    padding: EdgeInsets.all(paddingChild),
                     child : child,
                 ),
               );
