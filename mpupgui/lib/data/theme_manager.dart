20c619070e4858fa503f90eb70ff357767d775dd
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-06 15:38:36 +0100

    Added app themes enum and placeholder theme colors

--- mpupgui/lib/data/theme_manager.dart
+++ mpupgui/lib/data/theme_manager.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
 
+enum Theme {
+  dark,
+  light
+}
+
 class ThemeManager {
+
+  static Theme currentTheme = Theme.dark;
+
+  // App colors
+  static const Map<Theme, List<Color>> colorsApp = {
+    Theme.dark : [
+      Color.fromARGB(255, 23, 23, 23),
+      Color.fromARGB(255, 43, 43, 43),
+      Color.fromARGB(255, 54, 54, 54),
+      Color.fromARGB(255, 128, 128, 128),
+    ],
+    Theme.light : [
+      Color.fromARGB(255, 53, 53, 53),
+      Color.fromARGB(255, 73, 73, 73),
+      Color.fromARGB(255, 120, 120, 120),
+      Color.fromARGB(255, 160, 160, 160),
+    ]
+  };
+
+
   // TODO : Implement!
   static Color getColor(int level) {
     return Colors.blue; // For now, always return this color...
   }
+
 }
