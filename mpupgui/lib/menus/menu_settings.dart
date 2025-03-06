8bcaa6b16e5b455edeedc4c0a711b9148a479886
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-07 00:25:10 +0100

    Implemented basic settings menu logic

--- mpupgui/lib/menus/menu_settings.dart
+++ mpupgui/lib/menus/menu_settings.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:mpupgui/data/language_manager.dart';
+import 'package:mpupgui/data/theme_manager.dart';
+import 'package:mpupgui/widgets/mpup_button.dart';
+import 'package:mpupgui/widgets/mpup_container.dart';
+import 'package:mpupgui/widgets/mpup_text.dart';
 
 class SettingsMenu extends StatelessWidget {
   const SettingsMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Settings Menu");
+    return MagickaPupContainer(
+        paddingParent: 5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            MagickaPupButton(text: LanguageManager.getString("loc_theme_light"), onPressed: (){ThemeManager.setTheme(AppTheme.light);}, colorIndex: 2),
+            MagickaPupButton(text: LanguageManager.getString("loc_theme_dark"), onPressed: (){ThemeManager.setTheme(AppTheme.dark);}, colorIndex: 2),
+          ],
+        ),
+    );
   }
 }
 
