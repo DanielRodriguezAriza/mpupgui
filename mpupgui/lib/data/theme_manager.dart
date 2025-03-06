779463273faf50aafccb3555878259c5f512e685
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-07 00:24:46 +0100

    Renamed Theme enums to AppTheme to prevent shadowing Materials package's Theme object

--- mpupgui/lib/data/theme_manager.dart
+++ mpupgui/lib/data/theme_manager.dart
@@ -1,49 +1,49 @@
 import 'package:flutter/material.dart';
 
-enum Theme {
+enum AppTheme {
   dark,
   light
 }
 
-enum ThemeType {
+enum AppThemeType {
   image,
   text
 }
 
 class ThemeManager {
 
-  static Theme currentTheme = Theme.dark;
+  static AppTheme currentTheme = AppTheme.dark;
 
-  static const Map<ThemeType, Map<Theme, List<Color>>> colorsApp = {
-    ThemeType.image : {
-      Theme.dark: [
+  static const Map<AppThemeType, Map<AppTheme, List<Color>>> colorsApp = {
+    AppThemeType.image : {
+      AppTheme.dark: [
         Color.fromARGB(255, 23, 23, 23),
         Color.fromARGB(255, 43, 43, 43),
         Color.fromARGB(255, 54, 54, 54),
         Color.fromARGB(255, 128, 128, 128),
       ],
-      Theme.light: [
+      AppTheme.light: [
         Color.fromARGB(255, 53, 53, 53),
         Color.fromARGB(255, 73, 73, 73),
         Color.fromARGB(255, 120, 120, 120),
         Color.fromARGB(255, 160, 160, 160),
       ]
     },
-    ThemeType.text : {
-      Theme.dark: [
+    AppThemeType.text : {
+      AppTheme.dark: [
         Color.fromARGB(255, 255, 255, 255),
       ],
-      Theme.light: [
+      AppTheme.light: [
         Color.fromARGB(255, 0, 0, 0),
       ]
     }
   };
 
-  static void setTheme(Theme theme) {
+  static void setTheme(AppTheme theme) {
     currentTheme = theme;
   }
 
-  static bool isValidColorIndex(ThemeType themeType, Theme theme, int idx) {
+  static bool isValidColorIndex(AppThemeType themeType, AppTheme theme, int idx) {
     
     if(!colorsApp.containsKey(themeType)) {
       return false;
@@ -64,23 +64,23 @@ class ThemeManager {
     return true;
   }
 
-  static Color getColorInternal(ThemeType themeType, Theme theme, int level) {
+  static Color getColorInternal(AppThemeType themeType, AppTheme theme, int level) {
     if(!isValidColorIndex(themeType, theme, level)) {
       return Colors.black;
     }
     return colorsApp[themeType]![theme]![level];
   }
 
-  static Color getColor(ThemeType themeType, int level) {
+  static Color getColor(AppThemeType themeType, int level) {
     return getColorInternal(themeType, currentTheme, level);
   }
 
   static Color getColorImage(int level) {
-    return getColor(ThemeType.image, level);
+    return getColor(AppThemeType.image, level);
   }
 
   static Color getColorText(int level) {
-    return getColor(ThemeType.text, level);
+    return getColor(AppThemeType.text, level);
   }
 
 }
