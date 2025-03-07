5caafa4f3bef5bda2ef878c8723510a50e3c972a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-07 17:28:09 +0100

    Compiler menu now returns file processor widget

--- mpupgui/lib/menus/menu_fproc_compiler.dart
+++ mpupgui/lib/menus/menu_fproc_compiler.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mpupgui/widgets/file_processor.dart';
+import 'package:mpupgui/widgets/mpup_file_processor.dart';
 
 class CompilerMenu extends StatelessWidget {
   const CompilerMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      "Compiler Menu"
-    );
+    return const MagickaPupFileProcessor();
   }
 }
 
