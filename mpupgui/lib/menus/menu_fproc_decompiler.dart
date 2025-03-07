cb68acc001de78decc1fc6c13aca95a5547b994d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-07 17:28:54 +0100

    Decompiler menu now returns file processor widget

--- mpupgui/lib/menus/menu_fproc_decompiler.dart
+++ mpupgui/lib/menus/menu_fproc_decompiler.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mpupgui/widgets/file_processor.dart';
+import 'package:mpupgui/widgets/mpup_file_processor.dart';
 
 class DecompilerMenu extends StatelessWidget {
   const DecompilerMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      "Decompiler Menu"
-    );
+    return const MagickaPupFileProcessor();
   }
 }
 
