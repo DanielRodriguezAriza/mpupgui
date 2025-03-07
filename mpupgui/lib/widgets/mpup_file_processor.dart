ef8a6b2669db8f7a0bdc02996145c3c26ce4f628
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-07 17:27:35 +0100

    Started building basic layout for file processor

--- mpupgui/lib/widgets/mpup_file_processor.dart
+++ mpupgui/lib/widgets/mpup_file_processor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mpupgui/widgets/mpup_container.dart';
 
 import '../data/theme_manager.dart';
 import 'mpup_text.dart';
@@ -11,15 +12,22 @@ class MagickaPupFileProcessor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: ThemeManager.getColorImage(0),
-      child: Padding(
-        padding : const EdgeInsets.all(15),
-        child: Container(
-          color: ThemeManager.getColorImage(1),
-          child: const MagickaPupText(text : "Some test")
-        )
-      )
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+          child:Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.blue,
+            child: MagickaPupText(text: "0"),
+          )
+        ),
+        Expanded(
+        child:Container(
+            color: Colors.red,
+            child: MagickaPupText(text: "1")
+        )),
+      ],
     );
   }
 }
