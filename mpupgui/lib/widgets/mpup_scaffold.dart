ae0dc1c3a0469099988e82d271c4521798324160
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-08 21:52:48 +0100

    Created widget wrapper class for scaffolds
    TODO : Implement

---
+++ mpupgui/lib/widgets/mpup_scaffold.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MagickaPupScaffold extends StatelessWidget {
+  const MagickaPupScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
