085d98022ccf430692723e18d7c414536ea36f62
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-08 23:42:31 +0100

    Modified settings menu to use new navigation system and mpup scaffolding

--- mpupgui/lib/menus/menu_settings.dart
+++ mpupgui/lib/menus/menu_settings.dart
@@ -3,6 +3,7 @@ import 'package:mpupgui/data/language_manager.dart';
 import 'package:mpupgui/data/theme_manager.dart';
 import 'package:mpupgui/widgets/mpup_button.dart';
 import 'package:mpupgui/widgets/mpup_container.dart';
+import 'package:mpupgui/widgets/mpup_scaffold.dart';
 import 'package:mpupgui/widgets/mpup_text.dart';
 
 // TODO : Either make this a stateful widget or reload the "scene" / "page" / "menu" so that changes to visual settings are applied to all of the UI, for example theme change.
@@ -17,16 +18,18 @@ class SettingsMenu extends StatefulWidget {
 class _SettingsMenuState extends State<SettingsMenu> {
   @override
   Widget build(BuildContext context) {
-    return MagickaPupContainer(
-      paddingParent: 5,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          MagickaPupButton(text: LanguageManager.getString("loc_theme_light"), onPressed: (){setTheme(AppTheme.light);}, colorIndex: 2),
-          MagickaPupButton(text: LanguageManager.getString("loc_theme_dark"), onPressed: (){setTheme(AppTheme.dark);}, colorIndex: 2),
-        ],
-      ),
+    return MagickaPupScaffold(
+      child: MagickaPupContainer(
+        paddingParent: 5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            MagickaPupButton(text: LanguageManager.getString("loc_theme_light"), onPressed: (){setTheme(AppTheme.light);}, colorIndex: 2),
+            MagickaPupButton(text: LanguageManager.getString("loc_theme_dark"), onPressed: (){setTheme(AppTheme.dark);}, colorIndex: 2),
+          ],
+        ),
+      )
     );
   }
 
