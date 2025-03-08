ceb1114ed74eb0777292f47fd23b00ab5c3473ae
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-09 00:11:45 +0100

    Created utility page router class to transition with fade animation between menus
    This is far less annoying than the piece of shit the default transition animation does...

---
+++ mpupgui/lib/utility/plain_page_router.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PlainPageRouter extends PageRouteBuilder {
+  final Widget page;
+  PlainPageRouter({
+    required this.page,
+    super.settings
+  })
+  : super(
+    pageBuilder: (context, animation, secondaryAnimation) => page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return FadeTransition(
+        opacity: animation,
+        child: child,
+      );
+    }
+  );
+}
