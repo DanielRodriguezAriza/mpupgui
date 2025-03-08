82bcef0d1557d187b8b53fba09f6aee46cc84565
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-09 00:12:46 +0100

    Modified the main app class to use the new fade transition page router
    This change requires handling page routing manually with a custom function rather than whatever is offered by the default page router, altough I'm sure something could be done more cleanly if I dig deep enough in the docs...

--- mpupgui/lib/main.dart
+++ mpupgui/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:mpupgui/menus/menu_fproc_decompiler.dart';
 import 'package:mpupgui/menus/menu_init.dart';
 import 'package:mpupgui/menus/menu_main.dart';
 import 'package:mpupgui/menus/menu_settings.dart';
+import 'package:mpupgui/utility/plain_page_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,21 +16,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Disable the debug banner thing
       title: 'MagickaPUP GUI',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+
+      initialRoute: "/", // The init screen in this case
+      onGenerateRoute: (settings) {
+        return PlainPageRouter(
+            page: getPage(settings.name),
+            settings: settings,
+        );
+      },
+
+      // NOTE : Stuff ahead corresponds to the old routing system. Just ignore it for now until I clean this mess up...
       // home: const ScreenMain(),
-      debugShowCheckedModeBanner: false,
-      routes: {
+      /*routes: {
         "/": (context) => const InitScreen(),
         "/main": (context) => const MainMenu(),
         "/settings": (context) => const SettingsMenu(),
         "/compiler": (context) => const CompilerMenu(),
         "/decompiler": (context) => const DecompilerMenu(),
-      }
+      }*/
     );
+  }
+
+  Widget getPage(String? pathString) {
+    switch(pathString) {
+      case "/": return const InitScreen();
+      case "/main": return const MainMenu();
+      case "/compiler": return const CompilerMenu();
+      case "/decompiler": return const DecompilerMenu();
+      case "/settings": return const SettingsMenu();
+      default: return const InitScreen(); // NOTE : In the future, we could use a custom failure or 404 screen of sorts in this case, with a button to return to the main screen or whatever. For now, we just get sent back to the init screen and call it a day.
+    }
   }
 }
 
