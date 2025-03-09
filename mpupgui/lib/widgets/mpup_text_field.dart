5e331a487ceaa9bd744ccc22bc887faeba94eb20
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-09 03:35:54 +0100

    Added onEdit callback to mpup editable text field

--- mpupgui/lib/widgets/mpup_text_field.dart
+++ mpupgui/lib/widgets/mpup_text_field.dart
@@ -4,15 +4,18 @@ import 'package:mpupgui/data/theme_manager.dart';
 class MagickaPupTextField extends StatelessWidget {
 
   final TextEditingController controller;
+  final Function onEdit;
 
   const MagickaPupTextField({
     super.key,
     required this.controller,
+    required this.onEdit,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      onEditingComplete: (){onEdit();},
       controller: controller,
       keyboardType: TextInputType.text,
       textAlign: TextAlign.start,
