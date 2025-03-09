f49bdd4e4de5ad5d7bd6688d340c0c1a6beca690
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-09 01:55:16 +0100

    Converted file processor widget into a stateful widget and added input text field for input file path

--- mpupgui/lib/widgets/mpup_file_processor.dart
+++ mpupgui/lib/widgets/mpup_file_processor.dart
@@ -4,6 +4,55 @@ import 'package:mpupgui/widgets/mpup_container.dart';
 import '../data/theme_manager.dart';
 import 'mpup_text.dart';
 
+class MagickaPupFileProcessor extends StatefulWidget {
+  const MagickaPupFileProcessor({super.key});
+
+  @override
+  State<MagickaPupFileProcessor> createState() => _MagickaPupFileProcessorState();
+}
+
+class _MagickaPupFileProcessorState extends State<MagickaPupFileProcessor> {
+
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+            child: MagickaPupContainer(
+              colorIndex: 2,
+              child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      MagickaPupText(text: "Input file path: "),
+                      Expanded(
+                        child: TextField(
+                          controller: controller,
+                          keyboardType: TextInputType.text,
+                          textAlign: TextAlign.start
+                        )
+                      ),
+                    ],
+                  ),
+              ),
+            )
+        ),
+        Expanded(
+            child: MagickaPupContainer(
+              colorIndex: 2,
+              child: MagickaPupText(text: "0"),
+            )
+        ),
+      ],
+    );
+  }
+}
+
+/*
 class MagickaPupFileProcessor extends StatelessWidget {
 
   const MagickaPupFileProcessor({
@@ -18,7 +67,10 @@ class MagickaPupFileProcessor extends StatelessWidget {
         Expanded(
             child: MagickaPupContainer(
               colorIndex: 2,
-              child: MagickaPupText(text: "0"),
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: MagickaPupText(text: "Input file path")
+              ),
             )
         ),
         Expanded(
@@ -31,4 +83,4 @@ class MagickaPupFileProcessor extends StatelessWidget {
     );
   }
 }
-
+*/
