b8e2a20d83bbc8dffd2af23cbdc9cf6904b784e0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-09 03:12:33 +0100

    Implemented json reading and writing for settings manager

--- mpupgui/lib/data/settings_manager.dart
+++ mpupgui/lib/data/settings_manager.dart
@@ -1,6 +1,25 @@
+import 'dart:convert';
+import 'package:mpupgui/data/language_manager.dart';
+import 'package:mpupgui/data/mpup_manager.dart';
+import 'package:mpupgui/data/theme_manager.dart';
+import 'package:mpupgui/utility/file_handling.dart';
+
 class SettingsManager {
+
+  static const String settingsFile = "./mpupgui_settings.json";
+
   static void loadSettings() {
     try {
+      var str = readStringFromFile(settingsFile);
+      var data = jsonDecode(str);
+
+      Language language = Language.values.byName(data["Language"]);
+      AppTheme theme = AppTheme.values.byName(data["Theme"]);
+      String mpupPath = data["MagickaPupPath"];
+
+      LanguageManager.setLanguage(language);
+      ThemeManager.setTheme(theme);
+      MagickaPupManager.setMagickaPupPath(mpupPath);
 
     } catch(e) {
       print("SettingsManager.loadSettings() has failed : $e");
@@ -9,7 +28,13 @@ class SettingsManager {
 
   static void saveSettings() {
     try {
-
+      Map<String, dynamic> data = {
+        "Language" : LanguageManager.currentLanguage.name,
+        "Theme" : ThemeManager.currentTheme.name,
+        "MagickaPupPath" : MagickaPupManager.currentMagickaPupPath,
+      };
+      var str = jsonEncode(data);
+      writeStringToFile(settingsFile, str);
     } catch(e) {
       print("SettingsManager.saveSettings() has failed : $e");
     }
