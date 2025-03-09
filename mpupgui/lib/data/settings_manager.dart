20fcb65e028b936e46d048ecd06b8f9f436a9a7d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-09 02:58:00 +0100

    Created SettingsManager static class

---
+++ mpupgui/lib/data/settings_manager.dart
@@ -0,0 +1,17 @@
+class SettingsManager {
+  static void loadSettings() {
+    try {
+
+    } catch(e) {
+      print("SettingsManager.loadSettings() has failed : $e");
+    }
+  }
+
+  static void saveSettings() {
+    try {
+
+    } catch(e) {
+      print("SettingsManager.saveSettings() has failed : $e");
+    }
+  }
+}
