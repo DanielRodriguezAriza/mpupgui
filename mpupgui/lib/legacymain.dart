0b8b9f90321a7b17553ce983b9dc3af19cba8782
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-04 02:18:05 +0200

    Added legacy main backup file to repo

---
+++ mpupgui/lib/legacymain.dart
@@ -0,0 +1,66 @@
+// NOTE : This is a copy of the contents of the original main.dart file.
+// We'll come back to this code after making some experimental changes, which
+// require a completely new main file.
+/*
+import 'package:flutter/material.dart';
+import 'package:mpupgui/menus/menu_fproc_compiler.dart';
+import 'package:mpupgui/menus/menu_fproc_decompiler.dart';
+import 'package:mpupgui/menus/menu_init.dart';
+import 'package:mpupgui/menus/menu_main.dart';
+import 'package:mpupgui/menus/menu_mm.dart';
+import 'package:mpupgui/menus/menu_settings.dart';
+import 'package:mpupgui/utility/plain_page_router.dart';
+
+// NOTE : Made main into an async method so that process interop
+// doesn't shit its pants in flutter... yay...
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Disable the debug banner thing
+      title: 'MagickaPUP GUI',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+
+      initialRoute: "/", // The init screen in this case
+      onGenerateRoute: (settings) {
+        return PlainPageRouter(
+          page: getPage(settings.name),
+          settings: settings,
+        );
+      },
+
+      // NOTE : Stuff ahead corresponds to the old routing system. Just ignore it for now until I clean this mess up...
+      // home: const ScreenMain(),
+      /*routes: {
+        "/": (context) => const InitScreen(),
+        "/main": (context) => const MainMenu(),
+        "/settings": (context) => const SettingsMenu(),
+        "/compiler": (context) => const CompilerMenu(),
+        "/decompiler": (context) => const DecompilerMenu(),
+      }*/
+    );
+  }
+
+  Widget getPage(String? pathString) {
+    switch(pathString) {
+      case "/": return const InitScreen();
+      case "/main": return const MainMenu();
+      case "/compiler": return const CompilerMenu();
+      case "/decompiler": return const DecompilerMenu();
+      case "/settings": return const SettingsMenu();
+      case "/mod-manager" : return const ModManagerMenu();
+      default: return const InitScreen(); // NOTE : In the future, we could use a custom failure or 404 screen of sorts in this case, with a button to return to the main screen or whatever. For now, we just get sent back to the init screen and call it a day.
+    }
+  }
+}
+
+*/
