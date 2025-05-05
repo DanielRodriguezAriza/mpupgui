4568e0a080bae55bfa3137c1dd55df2cc015ed4a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-05 20:53:12 +0200

    Added test ui

--- mpupgui/lib/test_screen.dart
+++ mpupgui/lib/test_screen.dart
@@ -1,80 +1,125 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:mpupgui/data/theme_manager.dart';
 
 class TestScreen extends StatelessWidget {
   const TestScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return getMenuWidget();
+  }
+
+  Widget getWidgetOld() {
     return Scaffold(
-      body: Container(
-        color: Colors.green,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded (
-              child: Padding(
-                padding: const EdgeInsets.all(5),
-                child: Container(
-                  color: Colors.red,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(15),
-                          child: Container(
-                            color: Colors.blue,
-                            child: const Text("Hello World!"),
-                          )
-                        )
+        body: Container(
+            color: Colors.green,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded (
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Container(
+                        color: Colors.red,
+                        child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                  child: Padding(
+                                      padding: const EdgeInsets.all(15),
+                                      child: Container(
+                                        color: Colors.blue,
+                                        child: const Text("Hello World!"),
+                                      )
+                                  )
+                              ),
+                              Expanded(
+                                  child: Padding(
+                                      padding: const EdgeInsets.all(15),
+                                      child: Container(
+                                        color: Colors.blue,
+                                        child: const Text("Hello World!"),
+                                      )
+                                  )
+                              )
+                            ]
+                        ),
                       ),
-                      Expanded(
+                    )
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Text("Goodbye World!"),
+                ),
+                Row (
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
                           child: Padding(
-                              padding: const EdgeInsets.all(15),
-                              child: Container(
-                                color: Colors.blue,
-                                child: const Text("Hello World!"),
+                              padding: const EdgeInsets.all(5),
+                              child: ElevatedButton(
+                                onPressed: () async {
+                                  String process = "C:\\Users\\rodri\\Documents\\MagickaPUP\\MagickaPUP\\MagickaPUP\\bin\\Debug\\MagickaPUP.exe";
+                                  List<String> arguments = [
+                                    "-u",
+                                    "C:\\cosas\\barrel_petrol.xnb",
+                                    "C:\\cosas\\barrel_petrol.xnb.json2"
+                                  ];
+
+                                  var result = await Process.run(process, arguments);
+                                  print(result.stdout);
+                                },
+                                child: const Text("Hello!"),
                               )
                           )
                       )
                     ]
-                  ),
-                ),
-              )
-            ),
-            const Padding(
-              padding: EdgeInsets.all(5),
-              child: Text("Goodbye World!"),
-            ),
-            Row (
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  child: Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      String process = "C:\\MagickaPUP.exe";
-                      List<String> arguments = [
-                        "-u",
-                        "C:\\cosas\\barrel_petrol.xnb",
-                        "C:\\cosas\\barrel_petrol.xnb.json2"
-                      ];
-
-                      var result = await Process.run(process, arguments);
-                      print(result.stdout);
-                    },
-                    child: const Text("Hello!"),
-                  )
                 )
-                )
-              ]
+              ],
             )
-          ],
         )
+    );
+  }
+
+  Widget getMenuWidget() {
+    return Scaffold(
+      body: Column(
+        children: [
+          IntrinsicHeight (
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Container(
+                  color: Colors.red,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Text("Input Path")
+                      )
+                    ],
+                  )
+              )
+            )
+          ),
+          Expanded(
+              child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+                      color: Colors.red,
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Text("Input Path")
+                          )
+                        ],
+                      )
+                  )
+              )
+          )
+        ]
       )
     );
   }
+
 }
