76926ad48fff0fe97deb46ee562c117ff397c295
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-06 18:32:12 +0200

    Created simple container widget class

---
+++ mpupgui/lib/widgets/mpup/mpup_container_simple.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mpupgui/data/theme_manager.dart';
+
+class MagickaPupContainerSimple extends StatelessWidget {
+
+  final Widget? child;
+  final double? width;
+  final double? height;
+  final int level;
+
+  const MagickaPupContainerSimple({
+    super.key,
+
+    // Nullable properties.
+    // No assignment means that it is initialized to null by default. We don't use explicit assignment to null to a
+    // nullable type because it is considered redundant by Flutter standards since that is its default value.
+    this.child,
+    this.width,
+    this.height,
+    this.level = 0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    var themeData = ThemeManager.getCurrentThemeData();
+
+    return Padding(
+      padding: EdgeInsets.all(themeData.padding.outer),
+      child: Container(
+          decoration: BoxDecoration(
+              color: themeData.colors.image[level],
+              borderRadius: BorderRadius.circular(themeData.borderRadius)
+          ),
+          width: width,
+          height: height,
+          child: Padding(
+            padding: EdgeInsets.all(themeData.padding.inner),
+            child: child,
+          ),
+        )
+    );
+  }
+}
