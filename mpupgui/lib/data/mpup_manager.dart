7d30d31dc7a442a12554d2d5cc5d0c77b7b05714
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-08 01:42:04 +0200

    Made mpup manager into an static class (abstract final workaround for dart) and added getter method for mpup path

--- mpupgui/lib/data/mpup_manager.dart
+++ mpupgui/lib/data/mpup_manager.dart
@@ -1,7 +1,19 @@
-class MagickaPupManager {
+// NOTE : This is the best we can do to prevent this class from being instantiated and making it static.
+// abstract makes it so that we cannot have an explicit constructor unless we make an specific type that inherits from this one.
+// final makes it so that the type cannot be inherited.
+// With both keywords combined, we pretty much have a static class.
+// Not the best design, probably should make these non static in the future and add some instances of handler / manager classes
+// to some wrapper widget component so that we can make this into a dart lib or something, but for now this is ok since
+// the only consumer of this dart code is this flutter app, so yeah.
+
+abstract final class MagickaPupManager {
   static String currentMagickaPupPath = "";
 
   static void setMagickaPupPath(String path) {
     currentMagickaPupPath = path;
+  }
+
+  static String getMagickaPupPath() {
+    return currentMagickaPupPath;
   }
 }
