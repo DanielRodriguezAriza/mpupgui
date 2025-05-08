faa56ce31c23f200a8ab832ca399ab3924eddfa8
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-09 01:27:48 +0200

    Renamed tab selector dart file

--- mpupgui/lib/test_screen.dart
+++ mpupgui/lib/test_screen.dart
@@ -5,7 +5,7 @@ import 'package:mpupgui/data/mpup_manager.dart';
 import 'package:mpupgui/data/theme_manager.dart';
 import 'package:mpupgui/menus/menu_file_processor_generic.dart';
 import 'package:mpupgui/menus/menu_settings.dart';
-import 'package:mpupgui/widgets/mpup/tab/mpup_menu_selector_tabs.dart';
+import 'package:mpupgui/widgets/mpup/tab/mpup_tab_selector.dart';
 import 'package:mpupgui/widgets/mpup/container/mpup_background.dart';
 import 'package:mpupgui/widgets/mpup/container/mpup_container.dart';
 import 'package:mpupgui/widgets/mpup/container/mpup_container_simple.dart';
