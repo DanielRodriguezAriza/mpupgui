adeed848be2b2f2064e6104959229990612ab9ca
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-09 17:24:35 +0200

    Added dir and file picker functions to the file handling utility file

--- mpupgui/lib/utility/file_handling.dart
+++ mpupgui/lib/utility/file_handling.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
+import 'package:file_selector/file_selector.dart';
+
 void writeStringToFile(String fileName, String contents) {
   File file = File(fileName);
   file.writeAsStringSync(contents);
@@ -8,4 +11,25 @@ void writeStringToFile(String fileName, String contents) {
 String readStringFromFile(String fileName) {
   File file = File(fileName);
   return file.readAsStringSync();
+}
+
+Future<String?> pickDir() async {
+  String? directory = await getDirectoryPath();
+  return directory;
+}
+
+Future<List<String>?> pickFiles(bool allowMultiple, [List<String>? extensions]) async {
+  FilePickerResult? result = await FilePicker.platform.pickFiles(
+    type: extensions == null ? FileType.any : FileType.custom,
+    allowedExtensions: extensions,
+    allowMultiple: allowMultiple,
+  );
+  if(result != null) {
+    List<String> ans = [];
+    for(var file in result.files) {
+      ans.add(file.path!);
+    }
+    return ans;
+  }
+  return null;
 }
