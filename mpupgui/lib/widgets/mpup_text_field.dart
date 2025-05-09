8c1c1f3283a7fb0dc1c3ecfd88c962bb9ac9f0a7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-09 02:44:00 +0200

    Added background and border decor to mpup input text field widget

--- mpupgui/lib/widgets/mpup_text_field.dart
+++ mpupgui/lib/widgets/mpup_text_field.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mpupgui/data/theme_manager.dart';
+import 'package:mpupgui/widgets/mpup/container/mpup_container.dart';
 
 class MagickaPupTextField extends StatelessWidget {
 
@@ -14,6 +15,10 @@ class MagickaPupTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return getTextField();
+  }
+
+  Widget getTextField() {
     var themeData = ThemeManager.getCurrentThemeData();
     return TextField(
       onEditingComplete: onEdit,
@@ -22,6 +27,13 @@ class MagickaPupTextField extends StatelessWidget {
       textAlign: TextAlign.start,
       style: TextStyle(
         color: themeData.colors.text[0],
+      ),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: themeData.colors.image[0],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(themeData.borderRadius),
+        ),
       ),
     );
   }
