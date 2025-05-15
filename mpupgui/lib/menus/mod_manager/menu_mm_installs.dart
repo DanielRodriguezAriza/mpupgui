02490743ab3f070ef556dea096330ac1e3f4aa37
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-15 21:49:46 +0200

    Implemented function to load installs and implemented refresh button

--- mpupgui/lib/menus/mod_manager/menu_mm_installs.dart
+++ mpupgui/lib/menus/mod_manager/menu_mm_installs.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:mpupgui/data/mod_manager.dart';
+import 'package:mpupgui/utility/file_handling.dart';
 import 'package:mpupgui/widgets/mpup/container/mpup_background.dart';
 import 'package:mpupgui/widgets/mpup/container/mpup_container.dart';
 import 'package:mpupgui/widgets/mpup/io/mpup_button.dart';
@@ -28,12 +29,17 @@ class _ModManagerMenuInstallsState extends State<ModManagerMenuInstalls> {
     loadInstalls();
   }
 
+  // NOTE : We can either iterate all entries and just say if(entry is Directory)
+  // or use the .whereType<T>() function to filter and get only the entries that are of the specified type.
   void loadInstalls() {
+    installs.clear();
     var dir = Directory(ModManager.pathToInstalls);
     if(dir.existsSync()) {
-      var entries = dir.listSync();
-      for (var entry in entries) {
-        print("entry: ${entry.path}");
+      var entries = dir.listSync().whereType<Directory>();
+      for(var entry in entries) {
+        var name = pathName(entry.path);
+        installs.add(name);
+        // print(name);
       }
     }
   }
@@ -94,7 +100,7 @@ class _ModManagerMenuInstallsState extends State<ModManagerMenuInstalls> {
     return [
       getActionButtonWidget("Add new Install", null),
       getActionButtonWidget("Open Installs Directory", null),
-      getActionButtonWidget("Refresh", null),
+      getActionButtonWidget("Refresh", loadInstalls),
     ];
   }
 
