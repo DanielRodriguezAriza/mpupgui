5a8f351ed0da4178420390225af4ef1a0424f26f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-16 19:37:57 +0200

    Refactored mm installs menu widget to use the new generic mm list display

--- mpupgui/lib/menus/mod_manager/menu_mm_installs.dart
+++ mpupgui/lib/menus/mod_manager/menu_mm_installs.dart
@@ -2,37 +2,22 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:mpupgui/data/mod_manager.dart';
+import 'package:mpupgui/menus/mod_manager/menu_mm_generic_list_display.dart';
 import 'package:mpupgui/utility/file_handling.dart';
-import 'package:mpupgui/widgets/mpup/container/mpup_background.dart';
-import 'package:mpupgui/widgets/mpup/container/mpup_container.dart';
-import 'package:mpupgui/widgets/mpup/io/mpup_button.dart';
-import 'package:mpupgui/widgets/mpup/utility/mpup_scroller.dart';
-import 'package:mpupgui/widgets/mpup_scaffold.dart';
-import 'package:mpupgui/widgets/mpup_text.dart';
-import 'package:watcher/watcher.dart';
-import "package:open_filex/open_filex.dart";
 
-class ModManagerMenuInstalls extends StatefulWidget {
+class ModManagerMenuInstalls extends StatelessWidget {
   const ModManagerMenuInstalls({super.key});
 
   @override
-  State<ModManagerMenuInstalls> createState() => _ModManagerMenuInstallsState();
-}
-
-class _ModManagerMenuInstallsState extends State<ModManagerMenuInstalls> {
-
-  final ScrollController controller = ScrollController();
-  int installsCount = 0;
-  List<String> installsNames = [];
-  List<String> installsPaths = [];
-
-  @override
-  void initState() {
-    super.initState();
-    loadInstalls();
+  Widget build(BuildContext context) {
+    return ModManagerMenuGenericListDisplay(
+      name: "Installs",
+      directoryFilter: directoryFilter,
+      directoryGetter: directoryGetter
+    );
   }
 
-  bool isValidMagickaInstall(Directory directory) {
+  bool directoryFilter(Directory directory) {
     var entryFiles = directory.listSync().whereType<File>();
     for(var entryFile in entryFiles) {
       if(pathName(entryFile.path) == "Magicka.exe") {
@@ -42,137 +27,7 @@ class _ModManagerMenuInstallsState extends State<ModManagerMenuInstalls> {
     return false;
   }
 
-  // NOTE : We can either iterate all entries and just say if(entry is Directory)
-  // or use the .whereType<T>() function to filter and get only the entries that are of the specified type.
-  void loadInstalls() {
-    setState(() {
-      installsCount = 0;
-      installsNames.clear();
-      installsPaths.clear();
-    });
-    var dir = Directory(ModManager.pathToInstalls);
-    if(dir.existsSync()) {
-      var entries = dir.listSync().whereType<Directory>();
-      for(var entry in entries) {
-        if(isValidMagickaInstall(entry)) {
-          setState(() {
-            installsNames.add(pathName(entry.path));
-            installsPaths.add(entry.path);
-            installsCount += 1;
-          });
-        }
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return getWidget(context);
-  }
-
-  Widget getWidget(BuildContext context) {
-    return Scaffold(
-      body: MagickaPupBackground(
-        level: 0,
-        child: Column(
-          children: [
-            IntrinsicHeight(
-              child: MagickaPupContainer(
-                height: 60,
-                text: "Actions",
-                level: 2,
-                child: Row(
-                  children: getActionButtonWidgets(),
-                ),
-              ),
-            ),
-            Expanded(
-              child: MagickaPupContainer(
-                text: "Installs",
-                level: 2,
-                child: MagickaPupScroller(
-                  controller: controller,
-                  children: getInstallsWidgets(),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget getActionButtonWidget(String text, Function? action) {
-    return Expanded(
-      child: MagickaPupButton(
-        onPressed: (){
-          if(action != null) {
-            action();
-          }
-        },
-        child: MagickaPupText(
-          text: text,
-        ),
-      ),
-    );
-  }
-
-  List<Widget> getActionButtonWidgets() {
-    return [
-      getActionButtonWidget("Add new Install", null),
-      getActionButtonWidget("Open Installs Directory", null),
-      getActionButtonWidget("Refresh", loadInstalls),
-    ];
-  }
-
-  List<Widget> getInstallsWidgets() {
-    List<Widget> ans = [];
-    for(int i = 0; i < installsCount; ++i) {
-      ans.add(getInstallWidget(installsNames[i], installsPaths[i]));
-    }
-    return ans;
-  }
-
-  Widget getInstallWidget(String name, String path) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-      child: SizedBox(
-        width: 80,
-        height: 80,
-        child: MagickaPupContainer(
-          level: 1,
-          child: Row(
-            children: [
-              Expanded(
-                // flex: 9,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  child: MagickaPupText(
-                    isBold: true,
-                    text: name,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 50,
-                height: 50,
-                child: MagickaPupButton(
-                  level: 0,
-                  useAutoPadding: false,
-                  onPressed: () async {
-                    await OpenFilex.open(path);
-                  },
-                  child: const MagickaPupText(
-                    text: "...",
-                    fontSize: 10,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+  String directoryGetter() {
+    return ModManager.getPathToInstalls();
   }
 }
