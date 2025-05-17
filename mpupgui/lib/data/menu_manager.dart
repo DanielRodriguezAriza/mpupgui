0f3211951e8b516929338a9a3e50022fd166de1f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-18 01:52:40 +0200

    Added explanatory comment

--- mpupgui/lib/data/menu_manager.dart
+++ mpupgui/lib/data/menu_manager.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// NOTE : All of these static methods could actually just be free-standing functions...
+// But we're leaving it like this due to historical reasons, and being too lazy / having no time to fix this shit...
 abstract final class MenuManager {
 
   // Load directly the specified menu
