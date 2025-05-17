40919d6e9d5d7d3140f7b4405cb180d1c54abcfe
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-18 01:51:39 +0200

    Added functions to MenuManager to push and pop menus and removed old unused code

--- mpupgui/lib/data/menu_manager.dart
+++ mpupgui/lib/data/menu_manager.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
-class MenuManager {
+abstract final class MenuManager {
 
-  static int currentMenuIndex = 0;
-
-  static void setMenu(int i) {
-    currentMenuIndex = i;
-  }
-
+  // Load directly the specified menu
   static void loadMenu(BuildContext context, String menuName) {
     Navigator.pushNamedAndRemoveUntil(context, menuName, (_) => false);
   }
 
-  static Widget getMenu() {
-    return Text("The menu is $currentMenuIndex");
+  // Push the specified menu to enter it while keeping in memory the previously entered menu(s)
+  static void pushMenu(BuildContext context, String menuName) {
+    Navigator.pushNamed(context, menuName);
+  }
+
+  // Exit the current menu and go back to the previous one (if possible)
+  static void popMenu(BuildContext context) {
+    if(Navigator.canPop(context)) {
+      Navigator.pop(context);
+    }
   }
 }
