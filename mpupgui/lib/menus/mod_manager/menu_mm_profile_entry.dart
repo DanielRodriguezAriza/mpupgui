3fe66a2d711e1a299eeb990bf0edb263aaf30f1b
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-17 05:38:00 +0200

    Added basic buttons and events to profile entry widget

--- mpupgui/lib/menus/mod_manager/menu_mm_profile_entry.dart
+++ mpupgui/lib/menus/mod_manager/menu_mm_profile_entry.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:mpupgui/widgets/mpup/container/mpup_background.dart';
 import 'package:mpupgui/widgets/mpup/container/mpup_container.dart';
+import 'package:mpupgui/widgets/mpup/io/mpup_button.dart';
 import 'package:mpupgui/widgets/mpup_text.dart';
 
 class ModManagerMenuProfileEntry extends StatefulWidget {
-  const ModManagerMenuProfileEntry({super.key});
+
+  final Function? onApply;
+  final Function? onCancel;
+
+  const ModManagerMenuProfileEntry({
+    super.key,
+    this.onApply,
+    this.onCancel,
+  });
 
   @override
   State<ModManagerMenuProfileEntry> createState() => _ModManagerMenuProfileEntryState();
@@ -14,11 +23,53 @@ class _ModManagerMenuProfileEntryState extends State<ModManagerMenuProfileEntry>
   @override
   Widget build(BuildContext context) {
     return MagickaPupBackground(
-      child: MagickaPupContainer(
-        level: 2,
-        child: MagickaPupText(
-          text: "hi",
-        ),
+      child: Column(
+        children: [
+          IntrinsicHeight(
+            child: MagickaPupContainer(
+              height: 60,
+              text: "Actions",
+              level: 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MagickaPupButton(
+                      onPressed: (){
+                        if(widget.onApply != null) {
+                          widget.onApply!();
+                        }
+                      },
+                      child: MagickaPupText(
+                        text: "Apply Changes"
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: MagickaPupButton(
+                      onPressed: (){
+                        if(widget.onCancel != null) {
+                          widget.onCancel!();
+                        }
+                      },
+                      child: MagickaPupText(
+                        text: "Cancel",
+                      )
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: MagickaPupContainer(
+              text: "Profile",
+              level: 2,
+              child: MagickaPupText(
+                text: "hi",
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
