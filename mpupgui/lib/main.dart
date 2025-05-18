c244019aa29343a6e09ffb19ee5fb4a1a4b31140
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-18 02:00:39 +0200

    Moved test menu to test directory

--- mpupgui/lib/main.dart
+++ mpupgui/lib/main.dart
@@ -9,7 +9,7 @@ import 'package:mpupgui/menus/menu_main.dart';
 import 'package:mpupgui/menus/mod_manager/menu_mm.dart';
 import 'package:mpupgui/menus/menu_settings.dart';
 import 'package:mpupgui/menus/mod_manager/menu_mm_profile_entry.dart';
-import 'package:mpupgui/test_screen.dart';
+import 'package:mpupgui/menus/test/test_screen.dart';
 import 'package:mpupgui/utility/plain_page_router.dart';
 import 'package:window_size/window_size.dart';
 
