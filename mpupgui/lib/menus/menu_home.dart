244d1022fdf211544a8f5263ca9d904daa8dfa23
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-18 02:26:19 +0200

    Added system to fetch and display Magicka steam library banner image
    This way we do not infringe any laws, since we are not packing the game's image inside of the app's files...

--- mpupgui/lib/menus/menu_home.dart
+++ mpupgui/lib/menus/menu_home.dart
@@ -6,17 +6,43 @@ import 'package:mpupgui/widgets/mpup_text.dart';
 class HomeMenu extends StatelessWidget {
   const HomeMenu({super.key});
 
+  // The URL to the image that is displayed on the background of the steam library for Magicka 1
+  final String backgroundURL = "https://cdn.akamai.steamstatic.com/steam/apps/42910/library_hero.jpg";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: MagickaPupBackground(
         child: MagickaPupContainer(
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: MagickaPupText(
-              text: "Magicka Packer-Unpacker GUI",
-              fontSize: 24,
-            ),
+          level: 2,
+          child: Column(
+            children: [
+              MagickaPupContainer(
+                height: 60,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: MagickaPupText(
+                    text: "Magicka Packer-Unpacker GUI",
+                    fontSize: 28,
+                    isBold: true,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(backgroundURL),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
