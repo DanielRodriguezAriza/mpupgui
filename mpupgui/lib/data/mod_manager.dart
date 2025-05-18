a3a3d154b8e02b6d597f05f7d11cf43edd28a28a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-18 22:03:31 +0200

    Added regions and new properties for profile editing to mod manager class

--- mpupgui/lib/data/mod_manager.dart
+++ mpupgui/lib/data/mod_manager.dart
@@ -1,4 +1,6 @@
 class ModManager {
+  // region Mod Manager Config / Paths
+
   static String pathToInstalls = "./installs";
   static String pathToMods = "./mods";
   static String pathToProfiles = "./profiles";
@@ -26,4 +28,19 @@ class ModManager {
   static String getPathToProfiles() {
     return pathToProfiles;
   }
+
+  // endregion
+
+  // region Runtime Info
+
+  static late String selectedProfileName;
+  static late bool selectedProfileIsNew;
+
+  static void setSelectedProfile(String name, bool isNew) {
+    selectedProfileName = name;
+    selectedProfileIsNew = isNew;
+  }
+
+  // endregion
+
 }
