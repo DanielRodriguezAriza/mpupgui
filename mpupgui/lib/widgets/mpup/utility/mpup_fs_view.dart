b096a2409edd148d6de5a8f9903d7182af45d722
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-19 16:28:32 +0200

    Created mpup file system view widget
    This widget offers a scrollable area to display elements from the file system that meet a specific query.

---
+++ mpupgui/lib/widgets/mpup/utility/mpup_fs_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// This widget class offers a list view of elements from the file system.
+// Allows displays both files and directories that match the specified condition
+// to check whether they should be displayed or not.
+// A directory watcher is used to update events in real time.
+
+// TODO : Implement!!!
+
+class MagickaPupFileSystemView extends StatefulWidget {
+  const MagickaPupFileSystemView({super.key});
+
+  @override
+  State<MagickaPupFileSystemView> createState() => _MagickaPupFileSystemViewState();
+}
+
+class _MagickaPupFileSystemViewState extends State<MagickaPupFileSystemView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
