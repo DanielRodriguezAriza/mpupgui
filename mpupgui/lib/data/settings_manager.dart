15a27d820b7ab179b1dbfc32fa3b0c7a62186ce0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-19 04:31:11 +0200

    Reworked SettingsManager class to use the new settings data class for loading and saving of settings

--- mpupgui/lib/data/settings_manager.dart
+++ mpupgui/lib/data/settings_manager.dart
@@ -1,74 +1,39 @@
 import 'dart:convert';
+import 'package:mpupgui/data/app_settings_data.dart';
 import 'package:mpupgui/data/language_manager.dart';
 import 'package:mpupgui/data/mod_manager.dart';
 import 'package:mpupgui/data/mpup_manager.dart';
 import 'package:mpupgui/data/theme_manager.dart';
 import 'package:mpupgui/utility/file_handling.dart';
 
-class SettingsManager {
+abstract final class SettingsManager {
 
   static const String settingsFile = "./mpupgui_settings.json";
 
-  static T? loadValue<T>(var dict, String path) {
-    try {
-      T ans = dict[path];
-      return ans;
-    } catch(e) {
-      print("SettingsManager.loadValue() has failed : $e");
-      return null;
-    }
-  }
-
   static void loadSettings() {
-    try {
-      var str = readStringFromFile(settingsFile);
-      var data = jsonDecode(str);
+    AppSettingsData settingsData = AppSettingsData();
+    settingsData.readFromFile(settingsFile);
 
-      // Load Language
-      String languageStr = loadValue(data, "Language") ?? "english";
-      Language language = Language.values.byName(languageStr);
-      LanguageManager.setLanguage(language);
-
-      // Load App Theme
-      String themeStr = loadValue(data, "Theme") ?? "dark";
-      AppTheme theme = AppTheme.values.byName(themeStr);
-      ThemeManager.setTheme(theme);
-
-      // Load MagickaPup Path
-      String mpupPath = loadValue(data, "MagickaPupPath") ?? "./MagickaPUP.exe";
-      MagickaPupManager.setMagickaPupPath(mpupPath);
-
-      // Load path to installs
-      String pathToInstalls = loadValue(data, "pathToInstalls") ?? "./installs";
-      ModManager.setPathToInstalls(pathToInstalls);
-
-      // Load path to mods
-      String pathToMods = loadValue(data, "pathToMods") ?? "./mods";
-      ModManager.setPathToMods(pathToMods);
-
-      // Load path to profiles
-      String pathToProfiles = loadValue(data, "pathToProfiles") ?? "./profiles";
-      ModManager.setPathToProfiles(pathToProfiles);
-
-    } catch(e) {
-      print("SettingsManager.loadSettings() has failed : $e");
-    }
+    LanguageManager.setLanguage(settingsData.language);
+    ThemeManager.setTheme(settingsData.theme);
+    MagickaPupManager.setMagickaPupPath(settingsData.pathToMagickaPup);
+    ModManager.setPathToMagickCowModManager(settingsData.pathToMagickCowModManager);
+    ModManager.setPathToInstalls(settingsData.pathToInstalls);
+    ModManager.setPathToMods(settingsData.pathToMods);
+    ModManager.setPathToProfiles(settingsData.pathToProfiles);
   }
 
   static void saveSettings() {
-    try {
-      Map<String, dynamic> data = {
-        "Language" : LanguageManager.currentLanguage.name,
-        "Theme" : ThemeManager.currentTheme.name,
-        "MagickaPupPath" : MagickaPupManager.currentMagickaPupPath,
-        "pathToInstalls" : ModManager.getPathToInstalls(),
-        "pathToMods" : ModManager.getPathToMods(),
-        "pathToProfiles" : ModManager.getPathToProfiles()
-      };
-      var str = jsonEncode(data);
-      writeStringToFile(settingsFile, str);
-    } catch(e) {
-      print("SettingsManager.saveSettings() has failed : $e");
-    }
+    AppSettingsData settingsData = AppSettingsData();
+
+    settingsData.language = LanguageManager.getLanguage();
+    settingsData.theme = ThemeManager.getTheme();
+    settingsData.pathToMagickaPup = MagickaPupManager.getMagickaPupPath();
+    settingsData.pathToMagickCowModManager = ModManager.getPathToMagickCowModManager();
+    settingsData.pathToInstalls = ModManager.getPathToInstalls();
+    settingsData.pathToMods = ModManager.getPathToMods();
+    settingsData.pathToProfiles = ModManager.getPathToProfiles();
+
+    settingsData.writeToFile(settingsFile);
   }
 }
