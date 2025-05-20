77b04aae91d62f6efef3ae52241250b6040975f1
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-20 23:47:28 +0200

    Created separate widget class to hold data for install entry widgets

---
+++ mpupgui/lib/menus/mod_manager/profile/widget_install_entry.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:mpupgui/data/theme_manager.dart';
+import 'package:mpupgui/widgets/mpup/container/mpup_container.dart';
+import 'package:mpupgui/widgets/mpup/io/mpup_button.dart';
+import 'package:mpupgui/widgets/mpup_text.dart';
+import 'package:open_filex/open_filex.dart';
+
+class InstallEntryWidget extends StatelessWidget {
+
+  final String text;
+  final String path;
+  final bool selected;
+  final VoidCallback onSelected;
+
+  const InstallEntryWidget({
+    super.key,
+    required this.text,
+    required this.path,
+    required this.selected,
+    required this.onSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return getWidget();
+  }
+
+  Widget getWidget() {
+    final AppThemeData themeData = ThemeManager.getCurrentThemeData();
+    final Widget child = selected ? Container(
+      width: 20,
+      height: 20,
+      decoration: BoxDecoration(
+        color: themeData.colors.image[3],
+        borderRadius: BorderRadius.circular(100),
+      ),
+    ) : Container();
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+      child: SizedBox(
+        width: 80,
+        height: 80,
+        child: MagickaPupContainer(
+          level: 1,
+          child: Row(
+            children: [
+              Expanded(
+                // flex: 9,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: MagickaPupText(
+                    isBold: true,
+                    text: text,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                child: SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: MagickaPupButton(
+                    level: 0,
+                    useAutoPadding: false,
+                    onPressed: () async {
+                      await OpenFilex.open(path);
+                    },
+                    child: const MagickaPupText(
+                      text: "...",
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                child: SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: MagickaPupButton(
+                    level: 0,
+                    useAutoPadding: false,
+                    onPressed: onSelected,
+                    child: child,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
