31821455f9fe32ca50ff8b51e04cb226aaa7a4bd
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-05-28 20:19:00 +0200

    Added support for image caching on home screen
    This allows the image for the home screen to be stored on a file so that it will be saved and loaded when required. If the network connection is down, then we can use the cached image to prevent having to download it again.

--- mpupgui/lib/menus/menu_home.dart
+++ mpupgui/lib/menus/menu_home.dart
@@ -1,13 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:mpupgui/widgets/mpup/container/mpup_background.dart';
 import 'package:mpupgui/widgets/mpup/container/mpup_container.dart';
 import 'package:mpupgui/widgets/mpup_text.dart';
+import 'package:http/http.dart' as dart_http;
 
 class HomeMenu extends StatelessWidget {
   const HomeMenu({super.key});
 
   // The URL to the image that is displayed on the background of the steam library for Magicka 1
   final String backgroundURL = "https://cdn.akamai.steamstatic.com/steam/apps/42910/library_hero.jpg";
+  final String backgroundPath = "./library_hero.jpg";
 
   // NOTE : If the image fetching fails, nothing bad really happens. The app just fails silently by getting a 404 since the
   // resource was not found, and the visual effect is the same as if no image was displayed, so that's ok for our purposes.
@@ -21,7 +25,7 @@ class HomeMenu extends StatelessWidget {
           level: 2,
           child: Column(
             children: [
-              MagickaPupContainer(
+              const MagickaPupContainer(
                 height: 60,
                 child: Align(
                   alignment: Alignment.center,
@@ -33,17 +37,35 @@ class HomeMenu extends StatelessWidget {
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(backgroundURL),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: FutureBuilder<File?>(
+                        future: getImage(backgroundPath, backgroundURL),
+                        builder: (context, snapshot) {
+                          if(snapshot.connectionState == ConnectionState.waiting) {
+                            return Container();
+                          } else {
+                            final imageData = snapshot.data;
+                            if(imageData == null) {
+                              return Container();
+                            } else {
+                              return Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: FileImage(imageData),
+                                  ),
+                                ),
+                              );
+                            }
+                          }
+                        },
                       ),
                     ),
-                  ),
+                  ],
                 ),
               ),
             ],
@@ -52,4 +74,47 @@ class HomeMenu extends StatelessWidget {
       ),
     );
   }
+
+  // NOTE : Old method, no proper caching implemented. Just exists so that
+  // you can remember that NetworkImage exists.
+  /*
+  ImageProvider<Object> getImage() {
+    File backgroundFile = File(backgroundCachePath);
+    if(backgroundFile.existsSync()) {
+      return FileImage(backgroundFile);
+    } else {
+      return NetworkImage(backgroundURL);
+    }
+  }
+  */
+
+  Future<File?> getImage(String cachePath, String url) async {
+    File? file = await getImageFromPath(cachePath) ?? await getImageFromNetwork(cachePath, url);
+    return file;
+  }
+
+  Future<File?> getImageFromPath(String path) async {
+    File file = File(path);
+    bool fileExists = await file.exists();
+    if(fileExists) {
+      return file;
+    } else {
+      return null;
+    }
+  }
+
+  Future<File?> getImageFromNetwork(String cachePath, String url) async {
+    final response = await dart_http.get(Uri.parse(url));
+    if(response.statusCode == 200) {
+      final bytes = response.bodyBytes;
+      File file = File(cachePath);
+      await file.writeAsBytes(bytes);
+      bool fileExists = await file.exists();
+      if(fileExists) {
+        return file;
+      } // else, failed to write to target file
+    } // else, failed to find the resource online
+    return null;
+  }
+
 }
