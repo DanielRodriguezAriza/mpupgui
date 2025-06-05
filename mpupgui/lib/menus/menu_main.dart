b14302888aed4df42b2ffa9ff93b36f8da2bef4d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-06-05 19:08:08 +0200

    Modified main menu widget to use CompilerMenu and DecompilerMenu widgets

--- mpupgui/lib/menus/menu_main.dart
+++ mpupgui/lib/menus/menu_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mpupgui/menus/menu_file_processor_generic.dart';
+import 'package:mpupgui/menus/menu_fproc_compiler.dart';
+import 'package:mpupgui/menus/menu_fproc_decompiler.dart';
 import 'package:mpupgui/menus/menu_home.dart';
 import 'package:mpupgui/menus/menu_settings.dart';
 import 'package:mpupgui/menus/mod_manager/menu_mm_installs.dart';
@@ -45,15 +47,11 @@ class MainMenu extends StatelessWidget {
           ),
           TabData(
             name: "Compiler",
-            widget: MagickaPupFileProcessorMenuGeneric(
-              extensions: ["json", "png"], // NOTE : The compiler can try to compile any type of asset. We could leave it at that for now, but in the future maybe it would make sense to limit it to formats that mpup actually understands? but with this, it becomes far more trivial to maintain these 2 programs as separate, since updates to compiler support should not affect functionality of the gui. We'll see what I do in the end idk. Anyway, for now, I'm adding json and png and then we'll add more stuff or we'll get rid of this shit idk...
-            ),
+            widget: CompilerMenu(),
           ),
           TabData(
             name: "Decompiler",
-            widget: MagickaPupFileProcessorMenuGeneric(
-              extensions: ["xnb"],
-            ),
+            widget: DecompilerMenu(),
           ),
           TabData(
             name: "Settings",
