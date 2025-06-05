58babbf78c9c0cb96e2f4013486cdd64347e9a75
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-06-05 19:07:28 +0200

    Modified CompilerMenu widget to use new mpup fproc widget

--- mpupgui/lib/menus/menu_fproc_compiler.dart
+++ mpupgui/lib/menus/menu_fproc_compiler.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:mpupgui/widgets/mpup_file_processor.dart';
-import 'package:mpupgui/widgets/mpup_scaffold.dart';
+import 'package:mpupgui/menus/menu_file_processor_generic.dart';
 
 class CompilerMenu extends StatelessWidget {
   const CompilerMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MagickaPupScaffold(
-      child: MagickaPupFileProcessor(
-        processFileLocString: "loc_compile",
-        processFileCmdString: "-p",
-        processFileExtString: "xnb",
-      )
+
+    // NOTE : The compiler can try to compile any type of asset.
+    // We could leave it at that for now, but in the future maybe it would make
+    // sense to limit it to formats that mpup actually understands? but with
+    // this, it becomes far more trivial to maintain these 2 programs as
+    // separate, since updates to compiler support should not affect
+    // functionality of the gui. We'll see what I do in the end idk.
+    // Anyway, for now, I'm adding json and png and then we'll add more
+    // stuff or we'll get rid of this shit idk...
+
+    return const MagickaPupFileProcessorMenuGeneric(
+      extensions: ["json", "png"],
+      executionArgument: "-p",
+      locString: "loc_compile",
     );
   }
 }
