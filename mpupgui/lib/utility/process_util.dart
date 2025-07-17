3a38cd0a666b0f640b17cea5acc3d31f21acb422
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-07-17 22:36:22 +0200

    Implemented logic for processStart() function with admin privilege support
    The processStart() function now executes the proxy process executor and uses it as a way to prompt the UAC and allow users to execute operations that require admin privileges, such as symlink creation.

--- mpupgui/lib/utility/process_util.dart
+++ mpupgui/lib/utility/process_util.dart
@@ -4,11 +4,14 @@
 
 import 'dart:io';
 
+import 'package:mpupgui/data/mod_manager.dart';
+import 'package:mpupgui/utility/file_handling.dart';
+
 // NOTE : This function is extremely unstable, actually it does not work on all windows versions, so better not to use it.
 // TODO : Discard this piece of shit and delete this at some point in the future...
 // if only win32 allowed us to create fucking symlinks as regular users, then none of this would be needed...
 // but it seems like Windows is the most retarded OS ever made...
-Future<Process> processStart(String executable, [List<String> arguments = const [], bool runAsAdmin = false]) async {
+Future<Process> processStartOld(String executable, [List<String> arguments = const [], bool runAsAdmin = false]) async {
   runAsAdmin = Platform.isWindows ? runAsAdmin : false; // Always assume that we don't need to run as admin on any platform other than windows... yeah, dirty hack, but that's because the only thing this app needs like this is fucking symlinks and that does not require admin permissions anywhere else...
   if(runAsAdmin) {
     String exec = "powershell";
@@ -26,6 +29,24 @@ Future<Process> processStart(String executable, [List<String> arguments = const 
         "RunAs",
       ],
       runInShell: true,
+    );
+  } else {
+    return Process.start(
+      executable,
+      arguments,
+      runInShell: true,
+    );
+  }
+}
+
+Future<Process> processStart(String executable, [List<String> arguments = const [], bool runAsAdmin = false]) async {
+  if(runAsAdmin) {
+    List<String> args = ["true", executable];
+    args.addAll(arguments);
+    return Process.start(
+      pathFix(ModManager.getPathToMagickCowModManagerProxy()),
+      args,
+      runInShell: false,
     );
   } else {
     return Process.start(
